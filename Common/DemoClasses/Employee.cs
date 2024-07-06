089d7718c75f8ee096fbc785b7a57d395d67e12f
IIKhairyII <kkhairy2144@gmail.com>
2024-07-06 23:35:58 +0300

    Demo final

--- Common/DemoClasses/Employee.cs
+++ Common/DemoClasses/Employee.cs
@@ -1,6 +1,50 @@
 ﻿namespace Common.DemoClasses
 {
-    internal class Employee
+    public class Employee
     {
+        public Employee(int id, string name, decimal salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        private int _id;
+        private string? _name;
+        private decimal _salary;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Invalid Id");
+                else
+                    _id = value;
+            }
+        }
+        public string? Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public decimal Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+        public int GetEmployeeId() => _id;
+        public void SetEmployeeId(int id)
+        {
+
+            if (id <= 0)
+                throw new ArgumentException("Invalid Id");
+            else
+                _id = id;
+        }
+        public override string ToString()
+        {
+            return $"Employee {Name} with id {Id} has a salary {Salary} EGP";
+        }
+
     }
 }
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -6,17 +6,28 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            TypeA a = new();
-            //TypeB b = new(); //Invalid as the class is internal
-            a._c = 20; //Valid as _c is public
-            //a._a = 50; // invalid as _a is private
-            //a._b = 60; // invalid as _b is protected and valid only for sub classes of TypeA
-            //a._d = 70; // Invalid as _d is internal
-            Structs myStruct = new();
-            myStruct.Id = 1;
-            myStruct.SetName("Khairy");
-            Console.WriteLine($"This is {myStruct.GetName()} with id = {myStruct.Id}");
-            //Console.WriteLine(myStruct.Name); //invalid as Name is internal 
+            try
+            {
+                TypeA a = new();
+                //TypeB b = new(); //Invalid as the class is internal
+                a._c = 20; //Valid as _c is public
+                           //a._a = 50; // invalid as _a is private
+                           //a._b = 60; // invalid as _b is protected and valid only for sub classes of TypeA
+                           //a._d = 70; // Invalid as _d is internal
+                Structs myStruct = new();
+                myStruct.Id = 1;
+                myStruct.SetName("Khairy");
+                Console.WriteLine($"This is {myStruct.GetName()} with id = {myStruct.Id}");
+                //Console.WriteLine(myStruct.Name); //invalid as Name is internal 
+                Employee employee = new(100, "Hany", 7500);
+                Console.WriteLine(employee.ToString());
+                employee.SetEmployeeId(20);
+                Console.WriteLine($"Employee Id changed to {employee.GetEmployeeId()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
